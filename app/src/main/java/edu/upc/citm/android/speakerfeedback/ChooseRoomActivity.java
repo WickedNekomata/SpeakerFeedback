aeeb69dc0ea37ddf549c89e3e60883759d9ecee1
Sandra Alvarez <sandruskiag@gmail.com>
2018-12-13 10:46:39 +0100

    Enter the ID of the room

--- app/src/main/java/edu/upc/citm/android/speakerfeedback/ChooseRoomActivity.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/ChooseRoomActivity.java
@@ -1,13 +1,29 @@
 package edu.upc.citm.android.speakerfeedback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ChooseRoomActivity extends AppCompatActivity {
+
+    private EditText edit_room;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_room);
+
+        edit_room = findViewById(R.id.edit_room);
+    }
+
+    public void onEnterRoom(View view) {
+        String name = edit_room.getText().toString();
+        
+        //Intent data = new Intent();
+        //data.putExtra("name", name);
+        //setResult(RESULT_OK, data);
+        //finish();
     }
 }
